67ea815472998f6557722b9f32fe7d9ff5ce11f3
SergeyAnosov <anosov-sa@yandex.ru>
2024-11-22 16:12:52 +0300

    HelloController

---
+++ src/main/java/istok/mw/javataco/JavaTacoApplication.java
@@ -0,0 +1,13 @@
+package istok.mw.javataco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaTacoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaTacoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/istok/mw/javataco/JavaTacoApplicationTests.java
@@ -0,0 +1,13 @@
+package istok.mw.javataco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaTacoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
